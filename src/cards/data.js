663079bbdb5bb1a27f28614e7203d0ab0f4fc70a
Prathamd123 <prathamdevatwal0@gmail.com>
2025-11-18 12:14:57 +0530

    live links added for projects

--- src/cards/data.js
+++ src/cards/data.js
@@ -4,6 +4,7 @@ export const projects = [
       description: "Salon Connect is a modern web platform that helps users discover top-rated salons, compare services, and book appointments instantly. The goal was to create a clean, premium, and intuitive interface where users can explore beauty services, read reviews, and book slots seamlessly.",
       src: "/p1.jpg",
       link: "https://github.com/prathamd123/chic-flow-booking",
+      live: "https://github.com/prathamd123/",
       color: "#BBACAF"
     },
     {
@@ -11,6 +12,7 @@ export const projects = [
       description: "PillPix AI is an AI-powered tool that analyzes medicine images and generates useful details such as name, category, ingredients, and usage information. Designed with a simple and intuitive UI, the app lets users upload a photo of a pill or medicine strip and instantly receive structured insights.",
       src: "/p2.png",
       link: "https://github.com/prathamd123/AI-PillPeer-Medicine-Analyzer",
+      live: "https://github.com/prathamd123/",
       color: "#977F6D"
     },
     {
@@ -18,6 +20,7 @@ export const projects = [
       description: "Cuberto's website is a modern and responsive website that showcases the work of Cuberto, a design and development studio. The goal was to create a clean, premium, and intuitive interface where users can explore the work of Cuberto.",
       src: "/p3.jpg",
       link: "https://github.com/prathamd123/cuberto-assignment",
+      live: "https://github.com/prathamd123/",
       color: "#C2491D"
     },
     {
@@ -25,6 +28,7 @@ export const projects = [
       description: "AlgoViz is a web-based tool that helps users explore and visualize tree data structures with real-time animations. It supports Binary Search Trees, AVL Trees, and B-Trees, allowing users to insert, delete, and search nodes while watching the operations unfold visually.",
       src: "/p4.png",
       link: "https://github.com/prathamd123/AlgoVizhttps://github.com/prathamd123/AlgoViz",
+      live: "https://github.com/prathamd123/",
       color: "#B62429"
     },
     {
@@ -32,6 +36,7 @@ export const projects = [
       description: "Real Trust is a modern business website designed for a consultancy and marketing agency. It features a clean professional layout with a strong focus on lead generation, including a responsive “Get a Free Consultation” form. The design highlights the company’s services, projects, and testimonials in a clear and trustworthy manner.",
       src: "/p5.png",
       link: "https://github.com/prathamd123/Flipr-Task",
+      live: "https://github.com/prathamd123/",
       color: "#88A28D"
     }
   ]
